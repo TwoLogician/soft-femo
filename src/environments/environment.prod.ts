aa1e91a94752f7200335b08c9f46c359c58456af
Ehiin <ehiin@Ehiin-MBP.local>
2017-12-10 14:49:01 +0700

    ios HomeScreen Icon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD8V9A3QdeE0QIzVE23bFBeep4zZDMpngU",
+    authDomain: "femo-8c846.firebaseapp.com",
+    databaseURL: "https://femo-8c846.firebaseio.com",
+    projectId: "femo-8c846",
+    storageBucket: "",
+    messagingSenderId: "183235004899"
+  }
 };
